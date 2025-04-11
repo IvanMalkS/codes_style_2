4ee530290f947f7bb0052d9ea722538857bbe2da
Van1s1mys <77716298+IvanMalkS@users.noreply.github.com>
2025-04-11 13:52:08 +0500

    Update container.h

--- 5/container.h
+++ 5/container.h
@@ -18,62 +18,62 @@
 #include <iostream>
 
 class Deque {
-private:
-    struct Node {
-        int value;
-        Node* prev;
-        Node* next;
-        Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+    private:
+        struct Node {
+            int value;
+            Node* prev;
+            Node* next;
+            Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+        };
+    
+        Node* front;
+        Node* back;
+        int size;
+    
+    public:
+        Deque();
+        ~Deque();
+    
+        void pushBack(int value);
+        void pushFront(int value);
+        int popBack();
+        int popFront();
+        int getBack(int index) const;
+        int getSize() const;
+        bool isEmpty() const;
+        void clear();
+        void display() const;
+        void displayReverse() const;
     };
-
-    Node* front;
-    Node* back;
-    int size;
-
-public:
-    Deque();
-    ~Deque();
-
-    void pushBack(int value);
-    void pushFront(int value);
-    int popBack();
-    int popFront();
-    int getBack(int index) const;
-    int getSize() const;
-    bool isEmpty() const;
-    void clear();
-    void display() const;
-    void displayReverse() const;
-};
-
-class List {
-private:
-    struct Node {
-        int value;
-        Node* prev;
-        Node* next;
-        Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+    
+    class List {
+    private:
+        struct Node {
+            int value;
+            Node* prev;
+            Node* next;
+            Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+        };
+    
+        Node* head;
+        Node* tail;
+    
+    public:
+        List();
+        ~List();
+    
+        void pushBack(int value);
+        void pushFront(int value);
+        int popBack();
+        int popFront();
+        bool isEmpty() const;
+        int getSize() const;
+        void clear();
+        void display() const;
+        void displayReverse() const;
+        void insertBeforeFifthFromEnd(const Deque& deque);
+    
+        int size;
     };
-
-    Node* head;
-    Node* tail;
-
-public:
-    List();
-    ~List();
-
-    void pushBack(int value);
-    void pushFront(int value);
-    int popBack();
-    int popFront();
-    bool isEmpty() const;
-    int getSize() const;
-    void clear();
-    void display() const;
-    void displayReverse() const;
-    void insertBeforeFifthFromEnd(const Deque& deque);
-
-    int size;
-};
 
 #endif // CONTAINER_H
